14b0b6ce331d1a067b052f45c3c5f4e809f82711
Jason Doe <148502754+ironstah@users.noreply.github.com>
2023-11-15 12:44:13 -0700

    Add modules to website

---
+++ public_html/server.js
@@ -0,0 +1,8 @@
+
+const mongoose = require('mongoose');
+const express = require('express')
+const cookieParser = require('cookie-parser');
+const webcam = require('webcam-easy');
+const app = express()
+
+app.listen();
